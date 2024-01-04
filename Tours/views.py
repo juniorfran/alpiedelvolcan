cf40d7c564c3451324f178b6549f7fe7e2f88578
Francisco Antonio Flores <49844386+juniorfran@users.noreply.github.com>
2024-01-04 11:40:41 -0600

    index credo

--- Tours/views.py
+++ Tours/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Tour
 
-# Create your views here.
+def index(request):
+    # Obtener todos los tours desde la base de datos
+    tours = Tour.objects.all()
+
+    # Puedes agregar más lógica aquí según tus necesidades
+
+    # Renderizar la plantilla 'index.html' con la lista de tours
+    return render(request, 'index.alpiedelvolcan.html', {'tours': tours})
